7224578c8a22069f9d96b4673b95c89720f913b2
q0r3y <uxng2a4du6u2@opayq.net>
2022-05-12 11:50:29 -0400

    Fixed copied text animation

--- controllers/resController.js
+++ controllers/resController.js
@@ -11,12 +11,13 @@ function resFormat(res, msg, fs) {
     'text/html': function () {
       res.send(`
             <!DOCTYPE html>
-            <style>.l:active {transform: translateY(4px);}
-            .a{animation: fadeOut ease 2s; animation-fill-mode: forwards;}
-            @keyframes fadeOut {0% {opacity:1;}100% {opacity:0;}}</style><script>
-            function c(e) {navigator.clipboard.writeText(e.innerText)
-            let f=document.getElementById('a');f.style.color='white'; 
-            f.classList.add('a');}</script><body style="background:black;">
+            <style>.l:active{transform:translateY(4px);}
+            .a{animation:fadeOut ease 2s;animation-fill-mode:forwards;}
+            @keyframes fadeOut{0%{opacity:1;}100%{opacity:0;}}</style><script>
+            function c(e) {navigator.clipboard.writeText(e.innerText);
+            let f=document.getElementById('a');f.style.color='white';
+            let n=f.cloneNode(true);f.parentNode.replaceChild(n,f);
+            n.classList.add('a');}</script><body style="background:black;">
             <p onclick="c(this)" class="l" style="font-size:${fs};color:white;text-align:center;
             word-break:break-all;margin:40vh 5vw 5vh 5vw;">${msg}</p>
             <p id="a" style="color:black;text-align:center;">Copied</p>
