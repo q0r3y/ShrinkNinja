d4234880776cb4397a57793779a723d9a4e3f391
q0r3y <uxng2a4du6u2@opayq.net>
2023-04-26 15:01:50 -0400

    Added QRcode generator

--- public/dev/scripts/index.js
+++ public/dev/scripts/index.js
@@ -1,12 +1,16 @@
-function copy(e) {
-  let shortLink = e.innerText;
-  if (e.innerText.length <= 12) shortLink = 'https://' + e.innerText;
-  navigator.clipboard.writeText(shortLink);
+function copyLink() {
+  let $shortLink = document.getElementById('link').innerText;
+  if ($shortLink.length <= 12) $shortLink = 'https://' + $shortLink;
+  navigator.clipboard.writeText($shortLink);
   const $copy = document.getElementById('copy');
   $copy.style.filter = 'invert(100%)';
   const $clone = $copy.cloneNode(true);
   $copy.parentNode.replaceChild($clone, $copy);
   $clone.classList.add('a');
+}
+
+function transferToGithub() {
+  window.location.href = 'https://github.com/q0r3y/ShrinkNinja';
 }
 
 function getRandomNumber(limit) {
@@ -20,27 +24,41 @@ function getRandomColor() {
   return 'hsl(' + h + 'deg,' + s + '%,' + l + '%)';
 }
 
-function setBackgroundColor() {
-  const $randomColor = getRandomColor();
-  const $copyText = document.getElementById('copy');
-  const $background = document.getElementById('background');
-  const $svgs = document.getElementsByClassName('svg');
-  $background.style.backgroundColor = $randomColor;
-  $background.style.color = $randomColor;
-  $copyText.style.color = $randomColor;
-  for (i = 0; i < $svgs.length; i++) {
-    $svgs[i].style.fill = $randomColor;
-  }
-}
+function setColors() {
+  const randomColor = getRandomColor();
+  window.randomColor = randomColor;
 
-function transferToGithub() {
-  window.location.href = 'https://github.com/q0r3y/ShrinkNinja';
+  function setBgColor(color) {
+    const $background = document.getElementById('background');
+    $background.style.backgroundColor = color;
+    $background.style.color = color;
+  }
+
+  function setTextColor(color) {
+    const $copyText = document.getElementById('copy');
+    $copyText.style.color = color;
+  }
+
+  function setSvgColor(color) {
+    const $svgs = document.getElementsByClassName('svg');
+    for (i = 0; i < $svgs.length; i++) {
+      $svgs[i].style.fill = color;
+    }
+  }
+
+  setBgColor(randomColor);
+  setTextColor(randomColor);
+  setSvgColor(randomColor);
+  if (window.qrCode) {
+    window.qrCode.set({ foreground: randomColor });
+  }
 }
 
 async function requestCode() {
   const $linkText = document.getElementById('link');
   const $copyText = document.getElementById('copy');
   const $instText = document.getElementById('instructions');
+  const $qrCode = document.getElementById('qrcode');
   let inputData = window.location.href.slice(window.location.origin.length + 1);
 
   const checkForProtocol = function (str) {
@@ -50,6 +68,16 @@ async function requestCode() {
     } else {
       return 'https://' + str;
     }
+  };
+
+  const createQrCode = function (website) {
+    window.qrCode = new QRious({
+      element: document.getElementById('qrcode'),
+      backgroundAlpha: 0,
+      foreground: window.randomColor,
+      size: 300,
+      value: 'https://' + website,
+    });
   };
 
   if (inputData) {
@@ -69,6 +97,8 @@ async function requestCode() {
         $copyText.style.display = 'block';
         $linkText.innerText = data.longUrl || data.shortUrl;
         $linkText.style.display = 'block';
+        createQrCode(data.shortUrl);
+        $qrCode.style.display = 'block';
       } else {
         $instText.style.display = 'block';
       }
